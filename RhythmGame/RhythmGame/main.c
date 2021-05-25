d376d1655caa68659f06c65a3b90d3a686cf0708
RememberIOm <l412206@naver.com>
2021-05-25 18:23:05 +0900

    PLAY 화면 추가
    PLAY 버튼(PIN0)을 눌렀을 때, 화면이 공백이 되도록 함

--- RhythmGame/RhythmGame/main.c
+++ RhythmGame/RhythmGame/main.c
@@ -1,8 +1,8 @@
 #include "lcd.h"
 
-#define READY 0;
-#define PLAY 1;
-#define GAMEOVER 2;
+#define READY 0
+#define PLAY 1
+#define GAMEOVER 2
 
 unsigned char seed = 0;
 unsigned char state = READY;
@@ -22,28 +22,39 @@ int main(void)
 	cmd_lr( 0x3F ); //DISP ON
 	cmd_lr( 0xB8 );
 	cmd_lr( 0x40 );
-		
-	lcd_clear();
 	
 	TCNT1H = 0xB;
 	TCNT1L = 0xDC;
 	TCCR1A = 0x0;
-	TCCR1B = 0x01;
-		
-	TIMSK = (1<<TOIE1);
-	EIMSK = 0x01;
+	TCCR1B = 0x04;
+
+	EICRA = 0x66;
+	EICRB = 0x66;
+	EIMSK = 0x03;
 	sei();
 
 	while (1) {
 		if (state == READY) {
+			TIMSK = (1<<TOIE1);
 			lcd_clear();
 			startLcd();
-		}
-		else if (state = PLAY) {
 			
+			while (state == READY) {
+				_delay_us(1);
+			}
+		}
+		else if (state == PLAY) {
+			lcd_clear();
+			startLcd();
+			
+			while (state == PLAY) {
+				_delay_us(1);
+			}
 		}
 		else {
-			
+			while (state == GAMEOVER) {
+				_delay_us(1);
+			}
 		}
 	}
 }
@@ -54,25 +65,43 @@ void startLcd() {
 
 	GLCD_Line(ScreenBuffer, 0, 38, 63, 38);
 	GLCD_Line(ScreenBuffer, 58, 38, 58, 127);
+	
 	display_string(0, 0, "Score");
-	display_string(1, 0, "000");
+	display_char(1, 0, score / 100 + 0x30);
+	display_char(1, 1, score % 100 / 10 + 0x30);
+	display_char(1, 2, score % 10 + 0x30);
+	
 	display_string(3, 0, "Speed");
-	display_string(4, 0, "000");
-	display_string(0, 7, "1 - START");
-	display_string(1, 7, "2 - RESET");
-	display_string(2, 7, "3 - LANE 1");
-	display_string(3, 7, "4 - LANE 2");
-	display_string(4, 7, "5 - LANE 3");
-	display_string(5, 7, "6 - LANE 4");
+	display_char(4, 0, speed / 100 + 0x30);
+	display_char(4, 1, speed % 100 / 10 + 0x30);
+	display_char(4, 2, speed % 10 + 0x30);
+	
+	if (state == READY) {
+		display_string(0, 7, "1 - START");
+		display_string(1, 7, "2 - RESET");
+		display_string(2, 7, "3 - LANE 1");
+		display_string(3, 7, "4 - LANE 2");
+		display_string(4, 7, "5 - LANE 3");
+		display_string(5, 7, "6 - LANE 4");
+	}
 }
 
 ISR (TIMER1_OVF_vect) {
-	seed++;
+	seed = TCNT1L;
 }
 
+// START
 ISR (INT0_vect) {
 	if (state == READY) {
 		state = PLAY;
 	}
+	
+	TIMSK = (0<<TOIE1);
 }
 
+// RESET
+ISR (INT1_vect) {
+	if (state == GAMEOVER) {
+		state = READY;
+	}
+}
